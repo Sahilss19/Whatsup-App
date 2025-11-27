54d773a644f3e9f5541b8e243869cefdc80a7c71
Sahil19 <sahilkulria9988@gmail.com>
2025-11-27 19:45:22 +0530

    Clean up routing logic and improve code readability in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,43 @@
 import { useAuth } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
+import * as Sentry from "@sentry/react";
+
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
-// import { toast } from "react-hot-toast";
-import * as Sentry from "@sentry/react";
 import CallPage from "./pages/CallPage";
 
 const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
 const App = () => {
-  const { isSignedIn  ,isLoaded} = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
 
-  if (!isLoaded) {
-    return null;
-  }
+  if (!isLoaded) return null;
 
   return (
     <SentryRoutes>
+      {/* HOME */}
       <Route
         path="/"
-        element={isSignedIn ? <HomePage /> : <Navigate to={"/auth"} replace />}
+        element={isSignedIn ? <HomePage /> : <Navigate to="/auth" replace />}
       />
+
+      {/* AUTH */}
       <Route
         path="/auth"
-        element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+        element={!isSignedIn ? <AuthPage /> : <Navigate to="/" replace />}
       />
+
+      {/* CALL PAGE */}
       <Route
         path="/call/:id"
-        element={!isSignedIn ? <CallPage /> : <Navigate to={"/auth"} replace />}
+        element={isSignedIn ? <CallPage /> : <Navigate to="/auth" replace />}
       />
 
-      /* todo : call page pennding  -- okk okk for this page .... created call
-      page under pages .... */
-
-
+      {/* 404 HANDLER */}
       <Route
         path="*"
         element={
-          isSignedIn ? (
-            <Navigate to={"/"} replace />
-          ) : (
-            <Navigate to={"/auth"} replace />
-          )
+          isSignedIn ? <Navigate to="/" replace /> : <Navigate to="/auth" replace />
         }
       />
     </SentryRoutes>
@@ -49,6 +45,7 @@ const App = () => {
 };
 
 export default App;
+
 
 //first version of routing
 
